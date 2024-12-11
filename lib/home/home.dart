d18e22579d470264b3bd5517ac7524535fed7026
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-12-10 21:03:16 -0500

    adds app bar with the design and style of past gen project, adds flextube icon image

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -6,10 +6,38 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          color: Colors.blueGrey[200],
-          padding: EdgeInsets.all(20),
-          child: Text("Home Page")),
+      appBar: AppBar(
+        toolbarHeight: 40,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'img/brainflix-logo.png',
+              width: 25,
+            ),
+            SizedBox(width: 10),
+            Text(
+              "BrainFlix",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+              color: Colors.brown[200],
+              padding: EdgeInsets.all(20),
+              child: const Text("search bar")),
+          Container(
+            color: Colors.brown[300],
+            padding: EdgeInsets.all(20),
+            child: const Text("upload"),
+          )
+        ],
+      ),
     );
   }
 }
